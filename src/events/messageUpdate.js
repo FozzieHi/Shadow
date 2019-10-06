2bb53a8b68298955bd66ae30097ef8e0f66d1e81
Fozzie <fozzziedoesmc@gmail.com>
2019-10-06 20:53:50 +0100

    Fix footer error.

--- src/events/messageUpdate.js
+++ src/events/messageUpdate.js
@@ -25,9 +25,7 @@ client.on('messageUpdate', (oldMessage, newMessage) => {
                     if (logChannel !== undefined) {
                         const options = {
                             color: Configuration.lightOrangeColour,
-                            footer: {
-                                text: 'User ID: ' + newMessage.author.id
-                            },
+                            footer: 'User ID: ' + newMessage.author.id,
                             timestamp: true
                         };
 
