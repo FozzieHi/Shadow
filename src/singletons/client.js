05000d74be11c7909045dded060cf659dd384276
Fozzie <fozzziedoesmc@gmail.com>
2019-10-06 18:31:04 +0100

    Increase messageCacheLifetime to match the messageSweepInterval.

--- src/singletons/client.js
+++ src/singletons/client.js
@@ -1,4 +1,4 @@
 const discord = require('discord.js');
 const Configuration = require('../utils/Configuration.js');
 
-module.exports = new discord.Client({ fetchAllMembers: true, messageCacheMaxSize: 300, messageCacheLifetime: 60, messageSweepInterval: 3600, disabledEvents: Configuration.disabledEvents });
+module.exports = new discord.Client({ fetchAllMembers: true, messageCacheMaxSize: 300, messageCacheLifetime: 3600, messageSweepInterval: 3600, disabledEvents: Configuration.disabledEvents });
