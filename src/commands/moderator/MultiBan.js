c5d745322b20aa59a30156380d50d4449bddedd7
Fozzie <fozzziedoesmc@gmail.com>
2019-10-18 13:07:12 +0100

    Added MultiBan with the ability to ban multiple users in one message.

---
+++ src/commands/moderator/MultiBan.js
@@ -0,0 +1,34 @@
+const patron = require('patron.js');
+const StringUtil = require('../../utils/StringUtils.js');
+const ModerationService = require('../../services/ModerationService');
+
+class MultiBan extends patron.Command {
+    constructor() {
+        super({
+            names: ['multiban'],
+            groupName: 'moderator',
+            description: 'Ban multiple users, separate them with spaces.',
+            botPermissions: ['BAN_MEMBERS'],
+            args: [
+                new patron.Argument({
+                    name: 'users',
+                    key: 'users',
+                    type: 'user',
+                    example: 'User#0000 User#0001 User#0002',
+                    preconditions: ['nomoderator'],
+                    infinite: true
+                })
+            ]
+        });
+    }
+
+    async run(msg, args) {
+        await args.users.forEach(user => {
+            msg.guild.members.ban(user);
+            ModerationService.submitPunishment(msg.guild, msg.dbGuild, 'Ban', user, msg.author, '', msg.sender);
+        });
+        return msg.sender.reply(`Successfully multi-banned ${StringUtil.boldify(args.users.map(user => user.tag).join(", "))}.`);
+    }
+}
+
+module.exports = new MultiBan();
