8dc7e46d3d5ed64d89a28064eb12deb2893daf32
George <george@fozzie.dev>
2019-11-02 20:07:19 +0000

    Try disabling presence update.

--- src/events/presenceUpdate.js
+++ src/events/presenceUpdate.js
@@ -1,16 +1,16 @@
-const client = require('../singletons/client.js');
-const db = require('../database/index.js');
-const AutoModerationService = require('../services/AutoModerationService.js');
-const Logger = require('../utils/Logger.js');
-const StringUtils = require('../utils/StringUtils.js');
-
-client.on('presenceUpdate', async (oldPresence, newPresence) => {
-    (async () => {
-        const dbGuild = await db.guildRepo.getGuild(newPresence.guild.id);
-        if (dbGuild.autoMod.antiad) {
-            if (!StringUtils.isNullOrWhiteSpace(newPresence.activity) && !StringUtils.isNullOrWhiteSpace(newPresence.activity.state)) {
-                AutoModerationService.antiAdvertisingPresence(dbGuild, newPresence.member, newPresence.guild, newPresence.activity.state);
-            }
-        }
-    })().catch((err) => Logger.handleError(err));
-});
+// const client = require('../singletons/client.js');
+// const db = require('../database/index.js');
+// const AutoModerationService = require('../services/AutoModerationService.js');
+// const Logger = require('../utils/Logger.js');
+// const StringUtils = require('../utils/StringUtils.js');
+//
+// client.on('presenceUpdate', async (oldPresence, newPresence) => {
+//     (async () => {
+//         const dbGuild = await db.guildRepo.getGuild(newPresence.guild.id);
+//         if (dbGuild.autoMod.antiad) {
+//             if (!StringUtils.isNullOrWhiteSpace(newPresence.activity) && !StringUtils.isNullOrWhiteSpace(newPresence.activity.state)) {
+//                 AutoModerationService.antiAdvertisingPresence(dbGuild, newPresence.member, newPresence.guild, newPresence.activity.state);
+//             }
+//         }
+//     })().catch((err) => Logger.handleError(err));
+// });
