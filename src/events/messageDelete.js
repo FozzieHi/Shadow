4c7312246c4ca689035db5054d9aff49cc4939c7
George <george@fozzie.dev>
2020-08-12 21:36:40 +0100

    Log

--- src/events/messageDelete.js
+++ src/events/messageDelete.js
@@ -29,7 +29,7 @@ client.on('messageDelete', (message) => {
                         let fields = ['Action', `Message Deletion in ${message.channel}`,
                             'Content', message.content];
                         for (let i = 0; i < message.attachments.size; i++) {
-                            console.log(message.attachments[i]);
+                            console.log(message.attachments[i][0]);
                             fields.push(`Attachment ${i}`);
                             fields.push(`[View](${message.attachments[i][0].proxyURL})`)
                         }
