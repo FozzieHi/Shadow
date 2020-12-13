70d346471411eb8859e3debd6611e70cca07e96b
George <george@fozzie.dev>
2020-12-13 15:15:00 +0000

    Adjust message caching parameters

--- src/singletons/client.js
+++ src/singletons/client.js
@@ -1,4 +1,4 @@
 const discord = require('discord.js');
 const Configuration = require('../utils/Configuration.js');
 
-module.exports = new discord.Client({ fetchAllMembers: true, messageCacheMaxSize: 750, messageCacheLifetime: 604800, messageSweepInterval: 300, disabledEvents: Configuration.disabledEvents });
+module.exports = new discord.Client({ fetchAllMembers: true, messageCacheMaxSize: 450, messageCacheLifetime: 86400, messageSweepInterval: 200, disabledEvents: Configuration.disabledEvents });
