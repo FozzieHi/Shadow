125d95198a0620af8e6d1bb0102759c51c32077b
George <george@fozzie.dev>
2020-12-13 15:18:39 +0000

    Increase caching size

--- src/singletons/client.js
+++ src/singletons/client.js
@@ -1,4 +1,4 @@
 const discord = require('discord.js');
 const Configuration = require('../utils/Configuration.js');
 
-module.exports = new discord.Client({ fetchAllMembers: true, messageCacheMaxSize: 450, messageCacheLifetime: 86400, messageSweepInterval: 200, disabledEvents: Configuration.disabledEvents });
+module.exports = new discord.Client({ fetchAllMembers: true, messageCacheMaxSize: 1500, messageCacheLifetime: 604800, messageSweepInterval: 200, disabledEvents: Configuration.disabledEvents });
