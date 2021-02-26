e1dda1c67dc3dd36ffb2b09be0846f699f0090dd
George <george@fozzie.dev>
2021-02-26 16:00:04 +0000

    Relocate intents in the client startup.

--- src/singletons/client.js
+++ src/singletons/client.js
@@ -1,4 +1,4 @@
 const discord = require('discord.js');
 const Configuration = require('../utils/Configuration.js');
 
-module.exports = new discord.Client({ fetchAllMembers: true, messageCacheMaxSize: 1500, messageCacheLifetime: 604800, messageSweepInterval: 200, ws: { intents: Configuration.intents }});
+module.exports = new discord.Client({ fetchAllMembers: true, messageCacheMaxSize: 1500, messageCacheLifetime: 604800, messageSweepInterval: 200, intents: Configuration.intents });
